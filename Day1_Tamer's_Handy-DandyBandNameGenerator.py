66787ebacdac257d2a27aba270b2658201d534ec
TamerJAhmad <tamer71395@gmail.com>
2024-09-26 12:02:13 -0400

    Add files via upload
    Day 1 - 
    I created a brand name generator app that asks questions and concatenates the responses to create an awesome band name! 
    skills learned: string manipulation, input function, variables, print function

---
+++ Day1_Tamer's_Handy-DandyBandNameGenerator.py
@@ -0,0 +1,7 @@
+print("Welcome to Tamer's Handy-Dandy Band Name Generator!")
+
+cityName = input("What is the name of the city you grew up in?\n")
+
+petSuperName = input("What is your pet's name? <or favorite superhero ;)>\n")
+
+print("Your band name could be " + cityName + " " + petSuperName + "!\n\n(Or you decide, it won't hurt my feelings)")
